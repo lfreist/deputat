913a95bee19fce610502efda340262ecc6138f44
lfreist <freist.leon@gmx.de>
2020-05-04 12:10:52 +0200

    version 0.2.5

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 
 setup(
     name='deputat',
-    version='0.2.4',
+    version='0.2.5',
     description='deputat overview',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/lfreist/deputat",
     author='lfreist',
     author_email='freist.leon@gmx.de',
-    packages=[r'deputat'],
+    packages=['deputat'],
     install_requires=[
               'PyQt5',
               ],
